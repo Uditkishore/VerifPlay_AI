1cc6f004c25e11d48b558e5af93054d42da71ed8
MariyamNaeem6 <mariyam_naeem@andgatetech.com>
2025-10-31 15:51:29 +0530

    Uncommented DATABASES section for Django

--- verif_play_ground/settings.py
+++ verif_play_ground/settings.py
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'verif_play_ground.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = config("DB_NAME", default="verif_playground")
